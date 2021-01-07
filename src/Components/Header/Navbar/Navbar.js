9ce985121d40db10fed64eec35e62bfd30544462
Joseito Fernandes <joseito.terence@gmail.com>
2021-01-07 21:22:05 +0530

    Components readded

---
+++ src/Components/Header/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <section>
+      <nav className="navbar">
+        <div className="navbar__items">
+          <NavItem link='/' text='Home' />
+          <NavItem link='/aboutus' text='About Us' />
+          <NavItem link='/products' text='Products' />
+        </div>
+      </nav>
+    </section>
+  );
+}
+
+
+function NavItem({ text, link }) {
+  return (
+    <NavLink
+      exact
+      activeClassName="navbar__item-active"
+      to={link}
+    >
+      {text}
+    </NavLink>
+  );
+}
+
+export default Navbar;
+
