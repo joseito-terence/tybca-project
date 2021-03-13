ace5ef71e356a75f30be3b9070f3a4fcd2708c64
Joseito Fernandes <joseito.terence@gmail.com>
2021-03-13 22:35:36 +0530

    ContactUs form placed into a separate module

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,28 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
-import emailjs from "emailjs-com";
 
 function Footer() {
-  const [error, setError] = useState();
-
-  const sendEmail = (event) => {
-    event.preventDefault();
-
-    emailjs.sendForm(
-      "bcaproject", 								// Service id (gmail)
-      "template_8omwup7", 					// Template id of the email.
-      event.target, 								// input
-      "user_gNtKuwdgegE9KyZs4NguC" 	// API key
-    )
-    .then(
-      (result) => console.log(result.text),
-      (error) => setError(error.text)
-    );
-
-    event.target.reset();
-  };
-
   return (
     <>
       <footer className="footer">
@@ -41,44 +21,8 @@ function Footer() {
           </ul>
         </div>
         <div className="footer__contact-form">
-          <h5>Contact</h5>
-          <form onSubmit={sendEmail}>
-            <input
-              className="form-control mb-2"
-              type="text"
-              name="fullName"
-              placeholder="Full Name"
-              pattern="[A-Za-z ]{1,32}"
-              required
-            />
-            <input
-              className="form-control mb-2"
-              type="text"
-              name="email"
-              placeholder="Email"
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-              required
-            />
-            <textarea
-              className="form-control mb-2"
-              name="message"
-              cols="30"
-              rows="4"
-              placeholder="Message"
-							maxLength='100'
-              required
-            ></textarea>
-            <input className="btn btn-dark" type="submit" value="Submit" />
-
-            {error && 
-              <div className="alert alert-danger alert-dismissible fade show m-3" role="alert">
-                {error}
-                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            }
-          </form>
+          <h5>Sell your products</h5>
+          <a href="#link-to-seller-side">Register your business.</a>
         </div>
         <div className="footer__details">
           <img
